2b26b15a42de8516280de010b1f266e0690888f7
Marcos <cicero.sec@gmail.com>
2022-04-26 11:36:23 -0300

    Update megasena-scraping-new_website.py

--- megasena-scraping-new_website.py
+++ megasena-scraping-new_website.py
@@ -18,8 +18,6 @@ from bs4 import BeautifulSoup as sp
 
 url = 'https://asloterias.com.br/lista-de-resultados-da-mega-sena'
 
-lista_final = []
-
 mega_dezenas = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0,
                 '9': 0, '10': 0, '11': 0, '12': 0, '13': 0, '14': 0, '15': 0,
                 '16': 0, '17': 0, '18': 0, '19': 0, '20': 0, '21': 0, '22': 0,
@@ -57,12 +55,9 @@ with open('todos_os_resultados.csv', 'w') as arquivo_com_resultados:
         dez6 = int(resultado[32:34])
         dezenas = f'{dez1},{dez2},{dez3},{dez4},{dez5},{dez6}'
         print(dezenas, file=arquivo_com_resultados)
-        lista_final.append(dezenas)
-
-for linhas in lista_final:
-    for dezena in linhas.split(','):
-        if dezena in mega_dezenas:
-            mega_dezenas[dezena] += 1
+        for dezena in dezenas.split(','):
+            if dezena in mega_dezenas:
+                mega_dezenas[dezena] += 1
 
 for valor_ordenado in sorted(mega_dezenas.values()):
     for dezena, vezes in mega_dezenas.items():
